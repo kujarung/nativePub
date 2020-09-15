3c7ea2eeeeb44110cb6d0789f186472420bef8ac
kujarung <ryan@bliex.com>
2020-09-15 10:35:18 +0900

    intro3 깨짐 수정, 메인 화면 드로워 에러 수정

--- App.js
+++ App.js
@@ -7,30 +7,23 @@
  */
 
 import React from 'react';
-import {
-  View,
-  Button,
-  Text,
-  SafeAreaView,
-} from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
-import { Intro1 } from 'screen/intro/Intro1'
-import { Intro2 } from 'screen/intro/Intro2'
-import { Intro3 } from 'screen/intro/Intro3'
-import { login } from 'screen/login'
-import { signupPop } from 'screen/signupPop'
-import { signupResult } from 'screen/signupResult'
-import { PretestIntro } from 'screen/pretest/PretestIntro'
-import PretestStack from 'screen/pretest/PretestStack'
+import {View, Button, Text, SafeAreaView} from 'react-native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
+import {Intro1} from 'screen/intro/Intro1';
+import {Intro2} from 'screen/intro/Intro2';
+import {Intro3} from 'screen/intro/Intro3';
+import {login} from 'screen/login';
+import {signupPop} from 'screen/signupPop';
+import {signupResult} from 'screen/signupResult';
+import {PretestIntro} from 'screen/pretest/PretestIntro';
+import PretestStack from 'screen/pretest/PretestStack';
 import InterestStack from 'screen/interest/InterestStack';
-import { CustomDrawerContent } from 'screen/components/CustomDrawerContent'
-import { Header } from 'screen/components/Header';
+import {CustomDrawerContent} from 'screen/components/CustomDrawerContent';
+import {Header} from 'screen/components/Header';
 
-function HomeScreen({ navigation }) {
-  return (
-    <Header/>
-  );
+function HomeScreen({navigation}) {
+  return <Header nav={navigation} />;
 }
 
 const Drawer = createDrawerNavigator();
@@ -38,23 +31,22 @@ const Drawer = createDrawerNavigator();
 const App = () => {
   return (
     <>
-    <NavigationContainer>
-      <Drawer.Navigator 
-        initialRouteName="Home"
-        drawerContent = { (props) =>  CustomDrawerContent(props) }
-      >
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Intro1" component={Intro1} />
-        <Drawer.Screen name="Intro2" component={Intro2} />
-        <Drawer.Screen name="Intro3" component={Intro3} />
-        <Drawer.Screen name="login" component={login} />
-        <Drawer.Screen name="signupPop" component={signupPop} />
-        <Drawer.Screen name="signupResult" component={signupResult} />
-        <Drawer.Screen name="pretestIntro" component={PretestIntro} />
-        <Drawer.Screen name="pretestStack" component={PretestStack} />
-        <Drawer.Screen name="interestStack" component={InterestStack} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Drawer.Navigator
+          initialRouteName="Home"
+          drawerContent={(props) => CustomDrawerContent(props)}>
+          <Drawer.Screen name="Home" component={HomeScreen} />
+          <Drawer.Screen name="Intro1" component={Intro1} />
+          <Drawer.Screen name="Intro2" component={Intro2} />
+          <Drawer.Screen name="Intro3" component={Intro3} />
+          <Drawer.Screen name="login" component={login} />
+          <Drawer.Screen name="signupPop" component={signupPop} />
+          <Drawer.Screen name="signupResult" component={signupResult} />
+          <Drawer.Screen name="pretestIntro" component={PretestIntro} />
+          <Drawer.Screen name="pretestStack" component={PretestStack} />
+          <Drawer.Screen name="interestStack" component={InterestStack} />
+        </Drawer.Navigator>
+      </NavigationContainer>
     </>
   );
 };
--- src/screen/intro/Intro3.js
+++ src/screen/intro/Intro3.js
@@ -1,31 +1,40 @@
 import React from 'react';
-import { View, Text, ImageBackground, TouchableOpacity, Dimensions } from 'react-native';
+import {
+  View,
+  Text,
+  ImageBackground,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
 import btn from 'assets/styleSheet/btn';
 import intro from 'assets/styleSheet/intro';
-import common from 'assets/styleSheet/common'
-import { mainBak } from 'assets/styleSheet/imageUtils';
+import common from 'assets/styleSheet/common';
+import {mainBak} from 'assets/styleSheet/imageUtils';
 import color from 'assets/styleSheet/color';
 
+const {width, height} = Dimensions.get('window');
+
 export const Intro3 = () => {
-  
   press = () => {
-    console.log(press)
-  }
+    console.log(press);
+  };
 
   return (
     <View style={common.mainContainer}>
-      <ImageBackground source={mainBak} style={intro.mainBackImg}/>
-      <View style={intro.bottomCard}>
+      <ImageBackground source={mainBak} style={intro.mainBackImg} />
+      <View
+        style={[
+          intro.bottomCard,
+          {position: 'absolute', bottom: 90, height: height - 140},
+        ]}>
         <View>
-          <Text style={[common.h3]}>
-            시작하기  
-          </Text>
+          <Text style={[common.h3]}>시작하기</Text>
         </View>
         <View style={intro.innerBottomText}>
           <Text style={[common.h6, {color: color.fontGray}]}>
-            지금보다 더 많은 영어 콘텐츠를{"\n"}
-            읽을 수 있으려면 {"\n"}
-            특별한 방법이 필요해요.  {"\n"}
+            지금보다 더 많은 영어 콘텐츠를{'\n'}
+            읽을 수 있으려면 {'\n'}
+            특별한 방법이 필요해요. {'\n'}
             어떻게 하면 되는지 같이 알아봐요
           </Text>
         </View>
@@ -33,9 +42,11 @@ export const Intro3 = () => {
 
       <TouchableOpacity onPress={press} style={btn.bottomBtnCon}>
         <View style={[btn.btn, btn.backgroundPrimaryBtn]}>
-          <Text style={[btn.btnFontWhite, btn.btnFont, common.fontBold]}>어떻게 하면 되나요?</Text>
+          <Text style={[btn.btnFontWhite, btn.btnFont, common.fontBold]}>
+            어떻게 하면 되나요?
+          </Text>
         </View>
       </TouchableOpacity>
     </View>
-  ) 
-}
+  );
+};
